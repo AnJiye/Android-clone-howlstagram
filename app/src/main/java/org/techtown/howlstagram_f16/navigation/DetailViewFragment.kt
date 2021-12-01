1e449e8168a79caa5212429f5bf76ba7a6242163
AnJiye <68097036+AnJiye@users.noreply.github.com>
2021-12-01 17:36:50 +0900

    Add like button

--- app/src/main/java/org/techtown/howlstagram_f16/navigation/DetailViewFragment.kt
+++ app/src/main/java/org/techtown/howlstagram_f16/navigation/DetailViewFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_detail.view.*
 import kotlinx.android.synthetic.main.item_detail.view.*
@@ -54,6 +55,7 @@ class DetailViewFragment : Fragment() {
 
         // RecyclerView를 사용할 때 메모리를 적게 사용하기 위해 CustomViewHolder 사용
         inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
+        var uid = FirebaseAuth.getInstance().currentUser?.uid
 
         // 화면을 띄울 때 데이터를 매핑해줌
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
@@ -71,13 +73,49 @@ class DetailViewFragment : Fragment() {
             // likes
             viewholder.detailviewitem_favoritecounter_textview.text = "Likes " + contentDTOs!![position].favoriteCount
 
-            // ProfileImage
-            Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl).into(viewholder.detailviewitem_profile_image)
+//            // ProfileImage
+//            Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl).into(viewholder.detailviewitem_profile_image)
+
+            // This code is when the button is clicked
+            viewholder.detailviewitem_favorite_imageview.setOnClickListener {
+                favoriteEvent(position)
+            }
+
+            // 좋아요를 누르면 하트가 채워지고 else 문은 하트가 다시 빈하트로 바꿀 수 있도록 해줌
+            // This code is when the page is loaded
+            if(contentDTOs!![position].favorites.containsKey(uid)) {
+                // This is like status
+                viewholder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite)
+            } else {
+                // This is unlike status
+                viewholder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite_border)
+            }
         }
 
         // 가지고 있는 데이터베이스 개수
         override fun getItemCount(): Int {
             return contentDTOs.size
+        }
+
+        // user의 정보를 가져와서 if는 이미 클릭되어있을 경우, else는 그렇지 않은 경우
+        fun favoriteEvent(position : Int) {
+            var tsDoc = firestore?.collection("images")?.document(contentUidList[position])
+            firestore?.runTransaction { transaction ->
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                var contentDTO = transaction.get(tsDoc!!).toObject(ContentDTO::class.java)
+
+                if (contentDTO!!.favorites.containsKey(uid)) {
+                    // When the button is clicked
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount - 1
+                    contentDTO?.favorites.remove(uid)
+                } else {
+                    // When the button is not clicked
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount + 1
+                    contentDTO?.favorites[uid!!] = true
+                }
+                // 서버로 다시 결과를 보내주는 역할
+                transaction.set(tsDoc, contentDTO)
+            }
         }
 
     }
