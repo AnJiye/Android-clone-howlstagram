2474d1ffb85daf973050ed4e01ee50ef3dbfdd06
AnJiye <68097036+AnJiye@users.noreply.github.com>
2021-12-01 20:09:33 +0900

    Add grid page

--- app/src/main/java/org/techtown/howlstagram_f16/navigation/GridFragment.kt
+++ app/src/main/java/org/techtown/howlstagram_f16/navigation/GridFragment.kt
@@ -4,16 +4,78 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_grid.view.*
 import org.techtown.howlstagram_f16.R
+import org.techtown.howlstagram_f16.navigation.model.ContentDTO
 
 class GridFragment : Fragment() {
+    var firestore : FirebaseFirestore? = null
+    var fragmentView : View? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_grid, container, false)
-        return view
+        fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_grid, container, false)
+        firestore = FirebaseFirestore.getInstance()
+        fragmentView?.gridfragment_recyclerview?.adapter = UserFragmentRecyclerViewAdapter()
+        fragmentView?.gridfragment_recyclerview?.layoutManager = GridLayoutManager(activity, 3)
+        
+        return fragmentView
+    }
+
+    // detail에서도 사용했던 RecyclerView를 위한 부분
+    inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+
+        init {
+            // 사용자가 올린 이미지만 가지고 올 수 있도록 쿼리를 만들어 줌
+            firestore?.collection("images")?.addSnapshotListener { querySnapshot, firebaseFirestore ->
+                // Sometimes, This code return null of querySnapshot when it signout
+                // querySnapshot이 null이면 종료, 그렇지 않으면 for 문에서 데이터를 받아와서 contentDTO에 넣어줌
+                if (querySnapshot == null) return@addSnapshotListener
+
+                // Get data
+                for(snapshot in querySnapshot.documents) {
+                    contentDTOs.add(snapshot.toObject(ContentDTO::class.java)!!)
+                }
+                // 새로고침
+                notifyDataSetChanged()
+            }
+        }
+        // width를 화면의 1/3만 가져와서 정사각형을 만든 후 return
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var width = resources.displayMetrics.widthPixels / 3
+
+            var imageView = ImageView(parent.context)
+            imageView.layoutParams = LinearLayoutCompat.LayoutParams(width, width)
+            return CustomViewHolder(imageView)
+        }
+
+        inner class CustomViewHolder(var imageView: ImageView) : RecyclerView.ViewHolder(imageView) {
+
+        }
+
+        // 데이터를 매핑하는 부분
+        // ImageView를 불러오고 Glide로 이미지를 다운로드
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var imageview = (holder as CustomViewHolder).imageView
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl).apply(
+                RequestOptions().centerCrop()).into(imageview)
+        }
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
     }
 }
